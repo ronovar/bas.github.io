56391ed392425359ae846a988804ed4a8894573b
ronovar <ronovar@gmail.com>
2024-03-24 21:10:05 +0100

    Update custom.js

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
     $('.download').click(function(e) {
         e.stopPropagation();
-       alert("This is a click!");
+        
+        var href = window.location.host + '/bythos.github.io/attachments/' + $(this).text().split(' (PDF)')[0] + '.pdf';
+        var link = $('<a href="http://' + href + '" />');
+        var file = $(this).text().split(' (PDF)')[0].replace('-', '').split(' ').join('').toLowerCase();
+        var download = $(this).find('span');
+
+        link.attr('target', '_blank');
+        console.log(href);
+        console.log(link);
+        console.log(file);
+        console.log(download);
     });
 });
