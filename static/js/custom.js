efdc41a6d5c1daf6d89d71632291313ed9daa0d7
ronovar <ronovar@gmail.com>
2024-03-24 21:44:55 +0100

    Update custom.js

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,14 +1,22 @@
 $(document).ready(function(){
     $('.download').click(function(e) {
         e.stopPropagation();
+
+        var origin   = window.location.origin;
+        var pathname = window.location.pathname;
         
-        var href = window.location.origin + '/attachments/' + $(this).text().split(' (PDF)')[0] + '.pdf';
+        if (pathname) {
+            var url  = pathname.split("/");
+            var href = origin.concat("/", url[1]);
+        } else {
+            var href = origin;
+        }
+
         var link = $('<a href="http://' + href + '" />');
         var file = $(this).text().split(' (PDF)')[0].replace('-', '').split(' ').join('').toLowerCase();
         var download = $(this).find('span');
 
         link.attr('target', '_blank');
         console.log(href);
-        console.log(file);
     });
 });
