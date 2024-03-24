bdd6bc06cc38861f684c12f23fa61586b16b02a1
ronovar <ronovar@gmail.com>
2024-03-24 21:16:49 +0100

    Update custom.js

--- static/js/custom.js
+++ static/js/custom.js
@@ -9,6 +9,6 @@ $(document).ready(function(){
 
         link.attr('target', '_blank');
         console.log(href);
-        console.log(window.location.origin);
+        console.log(window.location.href);
     });
 });
